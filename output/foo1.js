0080966a08fa8de7b4b92ffb37786cd4c579e480
handsomeX <1258849260@qq.com>
2022-08-27 11:49:31 +0800

    穿越门对照

---
+++ output/foo1.js
@@ -0,0 +1,6 @@
+const test = 123;
+const foo = 321;
+
+function test11(params) {
+  return params;
+}
---
+++ src/foo1.js
@@ -0,0 +1,7 @@
+const test = 123
+const foo = 321
+
+function test11(params) {
+    123
+    return params
+}
